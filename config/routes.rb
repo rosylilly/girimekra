db280b586c579b733141c0119bc88a55cce7910c
Sho Kusano <sho-kusano@space-pirates.jp>
2017-08-18 11:08:53 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 require 'sidekiq/web'
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
+
   match '/auth/:provider/callback', to: 'users#create', via: %i[get post]
 
   get '/login', to: 'users#new'
