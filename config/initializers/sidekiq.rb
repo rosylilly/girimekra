867f57616d92ac05616e17e746d77eb6ef6483a1
Sho Kusano <sho-kusano@space-pirates.jp>
2017-08-18 12:08:13 +0900

    Do not use connection pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,9 +5,9 @@ require 'sidekiq/api'
 Rails.application.config.active_job.queue_adapter = :sidekiq
 
 Sidekiq.configure_client do |config|
-  config.redis = Rails.application.redis
+  config.redis = Rails.application.redis_configure[:connection].merge(Rails.application.redis_configure[:pool])
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = Rails.application.redis
+  config.redis = Rails.application.redis_configure[:connection].merge(Rails.application.redis_configure[:pool])
 end
