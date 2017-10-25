c135c060f1753f8e4b31173826ffb35a1fc49121
Sho Kusano <sho-kusano@space-pirates.jp>
2017-10-25 15:15:44 +0900

    Retryable cleanupper

--- app/jobs/clean_status_job.rb
+++ app/jobs/clean_status_job.rb
@@ -3,18 +3,20 @@
 class CleanStatusJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id)
+  def perform(user_id, max_id: 0)
+    max_id = [max_id.to_i, 0].max
     @user = User.find(user_id)
 
+    options = { limit: 40 }
+    options[:max_id] = max_id if max_id > 0
+
+    sleep 1 # for mastodon performance
+
     statuses = @user.rest_client.statuses(@user.uid, limit: 40)
-
-    until statuses.size.zero?
-      statuses.each do |status|
-        DestroyStatusJob.perform_later(@user.id, status.id)
-      end
-      sleep 1
-
-      statuses = @user.rest_client.statuses(@user.uid, max_id: statuses.last.id, limit: 40)
+    statuses.each do |status|
+      DestroyStatusJob.perform_later(@user.id, status.id)
     end
+
+    CleanStatusJob.perform_later(user_id, max_id: statuses.last.id) until statuses.size.zero?
   end
 end
